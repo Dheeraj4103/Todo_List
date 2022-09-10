16a1ce4d2fe46fa86b68bd1035cd32559e61c356
Dheeraj4103 <dheerajjadhav4455@gmail.com>
2022-09-10 21:17:57 +0530

    Working on Add task form

--- src/createitem.js
+++ src/createitem.js
@@ -1,16 +1,26 @@
 import "./index.css";
 import "boxicons";
-
 import "./taskstyle.css";
+
+
+function Task(title, duedate, priority, status) {
+  this.title = title;
+  this.duedate = duedate;
+  this.priority = priority;
+  this.status = status;
+  return { title, duedate, priority, status };
+};
+
 
 
 
 function createTask(title) {
   const task = document.createElement("div");
   task.className = "task";
-  const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-  task.appendChild(checkbox);
+  const box = document.createElement("input");
+  box.className = "check-task";
+  box.type = "checkbox";
+  task.appendChild(box);
   const taskname = document.createElement("h2");
   taskname.innerHTML = title;
   task.append(taskname);
